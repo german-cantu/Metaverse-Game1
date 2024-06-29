e97e2e35fe8e9b9dcaed44a85a1a7b63c39ff830
Germán Cantú <german3000_25@hotmail.com>
2024-06-29 15:43:14 -0600

    Renombrar archivo con ruta inválida para Windows

--- Metaverse_Game1/Intermediate/Build/Android/a/Metaverse_Game1/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "/Users/germancantu/Documents/GitHub/Metaverse-Game1/Metaverse_Game1/Intermediate/Build/Android/a/Metaverse_Game1/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
